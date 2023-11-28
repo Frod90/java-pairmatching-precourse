47e45bf0552e62c6d4daa9da42b419d23eb3b814
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-29 08:46:40 +0900

    Feat: 기능 작동

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,11 @@
 package pairmatching;
 
+import pairmatching.controller.FunctionController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+
+		FunctionController functionController = new FunctionController();
+		functionController.run();
+	}
 }
