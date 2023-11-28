8ea3827c8a18df82e9c016f21a213d936a8fd662
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-11-29 00:27:28 +0900

    Feat: 코스 이름 검사 기능, 크루원 섞는 기능 구현

---
+++ src/main/java/pairmatching/domain/Crew.java
@@ -0,0 +1,39 @@
+package pairmatching.domain;
+
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import pairmatching.constant.Course;
+
+public class Crew {
+	private Course course;
+
+	public Crew(String courseName) {
+
+		validateCourseName(courseName);
+		findCourse(courseName);
+	}
+
+	private void validateCourseName(String courseName) {
+		if (!courseName.equals("백엔드") && !courseName.equals("프론트엔드")) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void findCourse(String courseName) {
+		for (Course eachCourse : Course.values()) {
+			if (eachCourse.getName().equals(courseName)) {
+				course = eachCourse;
+			}
+		}
+	}
+
+	public List<String> shuffleCrew() {
+		return Randoms.shuffle(course.getCrewNames());
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+}
